fda42e6407bf6827850b9f9f46e25a25ab8dca46
loic-roux <69loloro10@gmail.com>
2021-05-17 14:55:59 +0200

    feat: new sortq

--- advanced-algo/tp5/tri-bubble.py
+++ advanced-algo/tp5/tri-bubble.py
@@ -1,24 +1,22 @@
 import sys
 from os import path
+from time import time
 sys.path.append(path.abspath(__file__ + '/../../modules'))
 
 from tests import run
 
-def tri_iteratif(int_list):
-    for i in range(len(int_list)):
-        min_index = i
+def tri_bulle(int_list):
 
-        for j in range(i + 1, len(int_list)):
-            print(min_index)
-            if int_list[min_index] > int_list[j]:
-                min_index = j
-
-        int_list[i], int_list[min_index] = int_list[min_index], int_list[i]
+    n = len(int_list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if int_list[j] > int_list[j + 1]:
+                int_list[j], int_list[j + 1] = int_list[j + 1], int_list[j]
 
     return int_list
 
-# 1.0s
-run(tri_iteratif, [
+# 3.0s
+run(tri_bulle, [
     {'args': {'int_list': [14, 7, 3, 18, 23]}, 'result': [3, 7, 14, 18, 23]},
     {'args': {'int_list': [0.1, 4, 3, 3, 72]}, 'result': [0.1, 3, 3, 4, 72]},
     {'args': {'int_list': [120, 47, 3, 18, 1, 2.5]}, 'result': [1, 2.5, 3, 18, 47, 120]},
--- advanced-algo/tp5/tri-insert.py
+++ advanced-algo/tp5/tri-insert.py
@@ -4,21 +4,23 @@ sys.path.append(path.abspath(__file__ + '/../../modules'))
 
 from tests import run
 
-def tri_iteratif(int_list):
-    for i in range(len(int_list)):
-        min_index = i
+def tri_insert(int_list):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(int_list)):
 
-        for j in range(i + 1, len(int_list)):
-            print(min_index)
-            if int_list[min_index] > int_list[j]:
-                min_index = j
+        key = int_list[i]
 
-        int_list[i], int_list[min_index] = int_list[min_index], int_list[i]
-
-    return int_list
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >= 0 and key < int_list[j] :
+                int_list[j + 1] = int_list[j]
+                j -= 1
+        int_list[j + 1] = key
 
 # 1.0s
-run(tri_iteratif, [
+run(tri_insert, [
     {'args': {'int_list': [14, 7, 3, 18, 23]}, 'result': [3, 7, 14, 18, 23]},
     {'args': {'int_list': [0.1, 4, 3, 3, 72]}, 'result': [0.1, 3, 3, 4, 72]},
     {'args': {'int_list': [120, 47, 3, 18, 1, 2.5]}, 'result': [1, 2.5, 3, 18, 47, 120]},
