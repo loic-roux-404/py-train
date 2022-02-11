c38a37d165435fb44608f628a4e2cfb0f7a8e892
loic-roux-404 <loic.roux.404@gmail.com>
2022-02-11 16:26:33 +0100

    top

--- algo-graphes/libGraph.py
+++ algo-graphes/libGraph.py
@@ -10,13 +10,29 @@ class Graph(object):
         self.nodes = nodes
         self.gtype = gtype
 
-    def show(self):
+    def show(self) -> 'SVG':
         graph = pydot.Dot("graphic", graph_type=self.gtype, bgcolor="white")
 
         for child in self.nodes:
             graph = child.get_graph(graph)
 
         return SVG(data=graph.create_svg())
+
+    def get_by_tag(self, tag) -> 'Node':
+        #print("searching %s" % tag)
+        res = list(map(lambda x: x if x.tag == tag else None, self.nodes))
+        #print(self.nodes)
+        if len(res) > 0:
+            return res[0]
+
+        return None
+
+    def to_tags(self):
+        return self._to_tags(self.nodes)
+
+    @classmethod
+    def _to_tags(self, nodes: list['Node']) -> list:
+        return list(map(lambda x: x.tag, nodes))
 
 class Node():
     def __init__(self, tag: str):
@@ -35,14 +51,16 @@ class Node():
             graph.add_node(pydot.Node(tag))
 
             for c in childs:
+                if len(graph.get_edge(tag, c)) > 0:
+                    continue
                 graph.add_edge(pydot.Edge(tag, c, color="black"))
 
         return graph
 
-    def show(self):
+    def show(self) -> 'SVG':
         return SVG(data=self.get_graph().create_svg())
 
-    def is_reachable(self, n: 'Node', already_tested = []):
+    def is_reachable(self, n: 'Node', already_tested = []) -> bool:
         res = False
         for child in self.nodes:
             already_tested.append(child)
@@ -54,8 +72,19 @@ class Node():
 
         return res
 
+    def deep_search(self: 'Node', visited: list['Node'] = [], dest: 'Node' = None) -> list['Node']:
+        if self in visited or (dest != None and dest in visited):
+            return visited
+
+        visited.append(self)
+
+        for n in self.nodes:
+            visited = n.deep_search(visited, dest)
+
+        return visited
+
     def __dict__(self):
-        def to_child_tags(childs: list[Node]):
+        def to_child_tags(childs: list[Node]) -> list:
             return list(map(lambda x : x.tag, childs))
 
         node_dict = {self.tag: to_child_tags(self.nodes)}
@@ -70,9 +99,8 @@ class Node():
                 nodes_to_dict(n.nodes)
 
         nodes_to_dict(tmp_nodes)
+
         return node_dict
-
-
 
 # TODO create result with splitted graphs
 def matrix_to_graph(matrix: np.array, labels: list[str] = []) -> Graph:
@@ -98,5 +126,8 @@ def matrix_to_graph(matrix: np.array, labels: list[str] = []) -> Graph:
 
     return Graph(res)
 
-def dijkstra():
-    pass
+def dijkstra(src: Node, dest: Node) -> list['Node']:
+    s, lambd, p = [[], [], []]
+    print(s, lambd, p)
+    print(src, dest)
+
